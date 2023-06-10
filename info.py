8c6fc10e1d97a4129387a2fa1ae4561675f7424e
blackhobert <132829195+blackhobert@users.noreply.github.com>
2023-06-10 12:43:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,11 +19,11 @@ def is_enabled(value, default):
 
 # Bot information
 PORT = environ.get("PORT", "8080")
-WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
+WEBHOOK = bool(environ.get("WEBHOOK", False)) # for web support on/off
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '22871675'))
 API_HASH = environ.get('API_HASH', '6f1767e506f8bb3f02ecf1536a221292')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6265704731:AAGvO-PGo4y8RuB3X2woyiiJZ6xF5ehK-Zc')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6029989081:AAGZs6gvbuf1ljgd65WhG_01SUXJ_FKsBVc')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -33,7 +33,7 @@ BOT_START_TIME = time()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '658407166').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001929998125').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001683799628').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('-1001761742758')
@@ -48,7 +48,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
 MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "7"))
-START_MESSAGE = environ.get('START_MESSAGE', 'Hey {user}\n\nI AM {bot},\nI CAN PROVIDE NEWSPAPERS. \nFor THE HINDU send ZA and for THE INDIAN EXPRESS send ZB')
+START_MESSAGE = environ.get('START_MESSAGE', 'Hey {user}\n\nI AM {bot},\nI CAN PROVIDE NEWSPAPERS. \nFor THE HINDU send ZA\nFor THE INDIAN EXPRESS send ZB')
 BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", "𝙃𝙚𝙮 {query}! 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣")
 FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', '𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 @the_pdf_hindu 𝑻𝒐 𝑼𝒔𝒆 𝑻𝒉𝒊𝒔 𝑩𝒐𝒕')
 RemoveBG_API = environ.get("RemoveBG_API", "")
@@ -77,7 +77,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001876808025')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
